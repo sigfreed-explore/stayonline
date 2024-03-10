5a916f861f7f454173b4bb80ad0853c80c3f5982
Evan Rusmisel <evanrusmisel@gmail.com>
2024-03-09 18:11:08 -0800

    lined crap

--- lined/src/main.rs
+++ lined/src/main.rs
@@ -1,25 +1,40 @@
+use std::collections::BTreeMap;
 use serde::{Serialize, Deserialize};
-use gpsd_proto::Tpv;
 use uuid::Uuid;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct SyncMsg {
     tok: String,
-    tpvs: Vec<Tpv>,
+    tpvs: BTreeMap<Uuid, Tpv>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Tpv {
+    time: String,
+    lat: f64,
+    lon: f64,
+    // TODO: moar
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let tree = sled::open("lined")?.open_tree("tpvs")?;
-    // let _f = BloomFilter::with_rate(2f32.powi(-8), 256);
+    let mut tree = sled::open("lined")?.open_tree("tpvs")?;
+    let tok = std::env::var("TOK")?;
 
     let egressor = tokio::spawn(async move {
         let c = reqwest::Client::new();
         loop {
-            let msg = SyncMsg { tok: format!("TODO"), tpvs: vec![]};
-            let body = Vec::<u8>::new();
-            ciborium::into_writer(&msg, &mut body);
-            let res = c.post("https://important-crab-11.deno.dev/api/sync").body(body).send().await;
+            let msg = SyncMsg { tok: tok.clone(), tpvs: BTreeMap::new()};
+            let mut body = Vec::<u8>::new();
+            match ciborium::into_writer(&msg, &mut body) {
+                // TODO
+                Ok(_) => {},
+                Err(e) => {
+                    eprintln!("{}", e);
+                    continue;
+                },
+            };
+            let _res = c.post("https://important-crab-11.deno.dev/api/sync").body(body).send().await;
         }
     });
 
