15eba3a5c1725b2c1658b3f4098a6b67dd9a33e1
Evan Rusmisel <evanrusmisel@gmail.com>
2024-04-01 00:22:19 -0700

    misc

--- lined/src/main.rs
+++ lined/src/main.rs
@@ -1,6 +1,16 @@
 use std::collections::BTreeMap;
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
+use tokio::sync::Mutex;
+use std::sync::Arc;
+use ciborium::from_reader;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+  tok: String,
+  persist_dir: String,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct SyncMsg {
@@ -18,13 +28,26 @@ struct Tpv {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut tree = sled::open("lined")?.open_tree("tpvs")?;
-    let tok = std::env::var("TOK")?;
+    let args = Args::parse();
+    let tree = Arc::new(Mutex::new(sled::open(args.persist_dir)?.open_tree("tpvs")?));
 
     let egressor = tokio::spawn(async move {
         let c = reqwest::Client::new();
         loop {
-            let msg = SyncMsg { tok: tok.clone(), tpvs: BTreeMap::new()};
+            let mut tpvs = BTreeMap::new();
+            let t= tree.lock().await;
+            for e in t.iter() {
+              match e {
+                Ok((k, v)) => {
+                    // TODO: fail gracefully rather than `expect`ing
+                    let k = Uuid::from_slice(&k).expect("bad sled key");
+                    let v = from_reader(v.as_ref()).expect("bad sled val");
+                    tpvs.insert(k,v);
+                },
+                Err(e) => eprintln!("Error reading TPV: {}", e),
+              }
+            }
+            let msg = SyncMsg { tok: args.tok.clone(), tpvs };
             let mut body = Vec::<u8>::new();
             match ciborium::into_writer(&msg, &mut body) {
                 // TODO
@@ -39,7 +62,8 @@ async fn main() -> anyhow::Result<()> {
     });
 
     let creator = tokio::spawn(async move {
-        
+        // let t = tree.lock().await;
+        // t.insert("", "").expect("failed to write");
     });
 
     creator.await?;
